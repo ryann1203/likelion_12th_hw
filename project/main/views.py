6bda9e100a381b6227074575df5ef1e3755b7953
ryann1203 <oxford1203@naver.com>
2024-04-07 00:41:40 +0900

    Feat: 개인프로젝트 생성

--- project/main/views.py
+++ project/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'generation': 12,
+        'members': ['현아', '영심이', '티준'],
+        'info':{'weather': '좋음', 'feeling': '배고픔(?)', 'note': '아기사자 화이팅!'},
+        'sessions':['장고는 MTV 패턴을 기반으로 한 프레임워크이다.', 'MTV 패턴에는 Model, Template, View가 있다.',
+                   'Model은 데이터, View는 요청에 따른 적절한 로직 수행, Template은 인터페이스를 의미한다.']
+    }
+    return render(request, 'main/mainpage.html', context)
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
